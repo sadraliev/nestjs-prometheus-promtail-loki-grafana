49bfe15f620d8651ad15044f9d02e4ce0fb8654a
sadraliev <a.sadraliev@gmail.com>
2024-04-14 18:41:36 +0600

    chore: add logger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env.development.local'],
+    }),
+    LoggerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: async (configService: ConfigService) => {
+        return {
+          pinoHttp: {
+            transport:
+              configService.get('NODE_ENV') !== 'production'
+                ? { target: 'pino-pretty' }
+                : undefined,
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
+  private readonly logger = new Logger(AppService.name);
+
   getHello(): string {
-    return 'Hello World!';
+    this.logger.log('getHello started');
+    const message = 'Hello World!';
+    this.logger.warn('getHello finished');
+    this.logger.error('getHello failed');
+    this.logger.debug('getHello debug');
+    this.logger.verbose('getHello verbose');
+    this.logger.fatal('getHello fatal');
+    return message;
   }
 }
